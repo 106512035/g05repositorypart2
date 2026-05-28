f40a0b864a937aa9155c7cd47be6da09967d44f6
Thomas Nguyen <tn2thomasnguyen@gmail.com>
2026-05-28 22:56:17 +1000

    added a session login for manage.php and a logout php to allow the user to terminate the session and logout.

--- manage.php
+++ manage.php
@@ -1,3 +1,13 @@
+<?php
+session_start();
+if (!isset($_SESSION['logged_in']) || $_SESSION['logged_in'] !== true) {
+    header('Location: login.php');
+    exit;
+}
+?>
+
+
+
 <h1>HR Manager</h1>
 <p>Welcome, you are logged in!</p>
-<a href="login.php">Logout</a>
+<a href="logout.php">Logout</a>

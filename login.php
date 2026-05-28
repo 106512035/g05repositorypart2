c37ffa7569c48616a174f8578198714e87b190cb
Thomas Nguyen <tn2thomasnguyen@gmail.com>
2026-05-28 22:43:20 +1000

    added html for loginphp for the user to enter the details.

--- login.php
+++ login.php
@@ -22,3 +22,17 @@ if (isset($_POST['username']) && isset($_POST['password'])) {
     }
 }
 ?>
+
+
+<!DOCTYPE html>
+<html>
+<head><title>HR Login</title></head>
+<body>
+  <h2>HR Manager Login</h2>
+  <form method="POST" action="login.php">
+    <label>Username: <input type="text" name="username" required></label><br><br>
+    <label>Password: <input type="password" name="password" required></label><br><br>
+    <button type="submit">Login</button>
+  </form>
+</body>
+</html>

333068b9c87958c028e03a7209b52d9d5b661175
Bianca Z <106512035@student.swin.edu.au>
2026-06-01 18:42:29 +1000

    Added process_eoi.php's comments

--- process_eoi.php
+++ process_eoi.php
@@ -1,16 +1,26 @@
 <?php
+//Session is started to remember the data
 session_start();
+//Displays all error reportings for the CSS, but is turned off in production.
 error_reporting(E_ALL);
+
+//Ensures that process_eoi.php file is not being directly accessed and redirects the user to the apply.php page. Exit stops the rest of 
+//the code from being excecuted.
 if ($_SERVER["REQUEST_METHOD"] != "POST") {
     header("Location: apply.php");
     exit();
 }
+
+//This checks to see if a connection is possible with the database by using the data in the provided php.
 require_once("settingsapply.php");
 $conn = mysqli_connect($host, $username, $password, $database);
 if (!$conn) {
-    echo "<p>Database connection failed." . mysqli_connect_error() . "</p>"; //probably remove mysqlconnecterror after testing
+    error_log("Failed to connect to the database:" . mysqli_connect_error());
+    echo "<p>Something went wrong! Try again later.</p>"; 
+    exit();
 }
 
+//This creates a table if it does not already exist within the database.
 $createTable = "CREATE TABLE IF NOT EXISTS eoi (
     EOInumber INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     job_id VARCHAR(5) NOT NULL,
@@ -29,10 +39,14 @@ $createTable = "CREATE TABLE IF NOT EXISTS eoi (
     status ENUM('New', 'Current', 'Final') NOT NULL DEFAULT 'New'
 )";
 
+//If there is an error creating the table a message displayed and an error message is also logged in the server.
 if (!mysqli_query($conn, $createTable)) {
-    echo "<p>Error creating table: " . mysqli_error($conn) . "</p>";
+    error_log("Error creating table: " . mysqli_error($conn));
+    echo "<p>Something went wrong! Try again later.</p>"; 
 }
 
+//This sanitises the input of all data by removing unessacary spaces, slashes and handles the converting of html special characters, 
+//returning the data.
 function sanitise_input($data) {
     $data = trim($data);
     $data = stripslashes($data);
@@ -40,8 +54,10 @@ function sanitise_input($data) {
     return $data;
 }
 
+//This sends a posting request to the PHP file
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
+    //Sanitising all data inputs
     $jobid = sanitise_input($_POST["jobid"]);
     $fname = sanitise_input($_POST["firstname"]);
     $lname = sanitise_input($_POST["lastname"]);
@@ -52,15 +68,18 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $email = sanitise_input($_POST["email"]);
     $phone_no = sanitise_input($_POST["phoneno"]);
     $suburb = sanitise_input($_POST["suburb"]);
-    $gender = isset ($_POST["gender"]) ? sanitise_input($_POST["gender"]) : ""; //if no it sets it do an empty string NOT GOOD. but error message dispplays??
+    $gender = isset ($_POST["gender"]) ? sanitise_input($_POST["gender"]) : ""; //This sanitises the input but if no radio button is 
+    //selected then an empty string is used.
     $state = sanitise_input($_POST["state"]);
 
+    //Creates an array for potential selected options from the skill set and seperating them each with a comma.
     $skill_list = isset($_POST["skill"]) ? implode (", ", array_map('sanitise_input', $_POST["skill"])) : "";
 
+    //Defining all patterns for each error and its corresponding error message.
     $errors = [];
+    if (!preg_match("/^[a-zA-Z0-9]{5}$/", $jobid)) $errors[] = "Please enter a valid 5 digit ID!<br>";
     if (!preg_match("/[A-Za-z]{1,20}/", $fname)) $errors[] = "First name is required (only alphabetic characters)!<br>";
     if (!preg_match("/[A-Za-z]{1,20}/", $lname)) $errors[] = "Last name is required (only alphabetic characters)!<br>";
-    if (!preg_match("/^[a-zA-Z0-9]{5}$/", $jobid)) $errors[] = "Please enter a valid 5 digit ID!<br>";
     if (empty($dob)) $errors[] = "Date of Birth is required!<br>";
     if (empty($address)) $errors[] = "Address is required!<br>"; 
     if (!preg_match("/^[0-9]{4}$/", $postcode)) $errors[] = "A valid postcode is required!<br>";   
@@ -70,19 +89,23 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if (empty($gender)) $errors[] = "Gender is required!<br>";
     if (empty($state)) $errors[] = "State is required!<br>";
 
+    //Checks if errors were found during validation and if found displays these errors.
     if (!empty($errors)) {
         $_SESSION['errors'] = $errors;
         $_SESSION['form_data'] = $_POST;
         header('Location: apply.php');
         exit();
     }else{
+        //If there aren't any errors the data is inserted into the databases table.
         $sql = "INSERT INTO eoi (job_id, first_name, last_name, dob, gender, address, suburb, state, postcode, email, phone_no, skill_list, other_skills)
         VALUES ('$jobid', '$fname', '$lname', '$dob', '$gender', '$address', '$suburb', '$state', '$postcode', '$email', '$phone_no', '$skill_list', '$otherskills')";
+        //Redirects back to apply.php and displays the success message with the eoi number.
         if (mysqli_query($conn, $sql)){
             $eoi_number = mysqli_insert_id($conn);
             $_SESSION['success'] = "Form Submitted Successfully! Your EOInumber is: " . $eoi_number;
             header('Location: apply.php');
             exit();
+        // If errors were found then the errors are displayed.
         }else{
             $_SESSION['errors'][]="Error Submitting Form!";
             header('Location: apply.php');
